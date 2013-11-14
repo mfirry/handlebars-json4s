a064beff6a0a8e13790078840400ba2b9c5c1c38
Marco F <mfirry@gmail.com>
2013-11-14 10:28:01 +0100

    refactored

--- src/main/scala/com/github/mfirry/handlebars/json4s/JValueResolver.scala
+++ src/main/scala/com/github/mfirry/handlebars/json4s/JValueResolver.scala
@@ -13,27 +13,29 @@ import com.github.jknack.handlebars.ValueResolver._
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
-class JsonNodeValueResolver extends ValueResolver {
+class JValueResolver extends ValueResolver {
 
   def resolve(context: AnyRef, name: String): AnyRef = context match {
     case JArray(arr) =>
       arr.lift(name.toInt) getOrElse UNRESOLVED
     case json: JObject =>
       val resolved = json \ name
-      if (resolved == JNothing) {
-        UNRESOLVED
-      } else {
-        resolved
+      resolved match {
+        case JNothing => UNRESOLVED
+        case _ => resolved
       }
     case _ =>
       UNRESOLVED
   }
 
-  private def blah(jfield: (String, JValue)): Entry[String, AnyRef] = new SimpleImmutableEntry(jfield._1, resolve(jfield._2, jfield._1))
+  private def unpack(jfield: (String, JValue)): Entry[String, AnyRef] =
+    new SimpleImmutableEntry(jfield._1, resolve(jfield._2, jfield._1))
 
   def propertySet(context: AnyRef): JSet[Entry[String, AnyRef]] = context match {
     case JObject(obj) =>
-      obj map blah toSet : Set[Entry[String, AnyRef]]
+      obj map unpack toSet : Set[Entry[String, AnyRef]]
     case _ => Set.empty[Entry[String, AnyRef]]
   }
 }
+
+object INSTANCE
