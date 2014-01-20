a1c4d9ff242b6589c01cb9f0dea4c9a2addc5d1e
Marco F <mfirry@gmail.com>
2014-01-20 18:25:55 +0100

    tesst

--- src/test/scala/com/github/mfirry/handlebars/json4s/JValueResolverTest.scala
+++ src/test/scala/com/github/mfirry/handlebars/json4s/JValueResolverTest.scala
@@ -4,17 +4,26 @@ package handlebars.json4s
 import org.specs2.mutable._
 
 import org.json4s._
-import org.json4s.native.JsonMethods._
+import org.json4s.jackson.JsonMethods._
 import org.json4s.JsonDSL._
 
-import com.github.jknack.handlebars.Handlebars
+import com.github.jknack.handlebars.{ Handlebars, Context }
 
 class JValueResolverTest extends Specification {
 
    val handlebars = new Handlebars()
-   val root = Map("string" -> "abc", "int" -> 678, "long" -> 6789L, "float" -> 7.13f, "double" -> 3.14d, "bool" -> true)
-   
-   val context = Context.newBuilder(json).resolver(JValueResolver.INSTANCE).build
-   
+
+   "Parsing json" should {
+      "produce the expected output" in {
+          val json = ("string" -> "abc") ~
+            ("int" -> 678) ~ ("long" -> 6789L) ~
+            ("float" -> 7.13f) ~
+            ("double" -> 3.14d) ~ ("bool" -> true)
+
+          val context = Context.newBuilder(json).resolver(JValueResolver.INSTANCE).build
+          val template = handlebars.compileInline("{{string}} {{int}} {{long}} {{float}} {{double}} {{bool}}")
+
+          template.apply(context) must be_==("abc 678 6789 7.130000114440918 3.14 true")
+      }
+   }
   }
-  
